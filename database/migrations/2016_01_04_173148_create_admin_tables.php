6d159773f0cf0a57a7afbde3cea0ca770c6be079
南城 <lampxiezi@163.com>
2019-11-08 13:43:27 +0800

    更新

--- database/migrations/2016_01_04_173148_create_admin_tables.php
+++ database/migrations/2016_01_04_173148_create_admin_tables.php
@@ -124,22 +124,6 @@ class CreateAdminTables extends Migration
                 $table->timestamps();
             }
         );
-        Schema::create(
-            'modules',
-            function (Blueprint $table) {
-                $table->bigIncrements('id');
-                $table->string('title')->comment('模块名称');
-                $table->string('name')->unique()->comment('模块标识');
-                $table->string('version')->comment('版本号');
-                $table->tinyInteger('local')->comment('本地模块');
-                $table->tinyInteger('is_nav')->default(0)->comment('是否显示导航');
-                $table->tinyInteger('status')->default(0)->comment('是否上线');
-                $table->text('package')->comment('模块配置');
-                $table->tinyInteger('subscribe')->comment('权限配置');
-                $table->text('permissions')->comment('权限配置');
-                $table->timestamps();
-            }
-        );
     }
 
     /**
